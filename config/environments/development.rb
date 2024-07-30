3d3106a41cf7c1fbe72df325fd5e55dcee41e068
Wildan <wildanhuang@gmail.com>
2024-07-30 17:12:08 +0700

    add dev hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.hosts << "local-ubuntu:3000"
+  config.hosts << "sttbaptisbandung.ac.id"
+  config.hosts << "www.sttbaptisbandung.ac.id"
 end
