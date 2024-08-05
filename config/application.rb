b6b864867521ef35a6b47e9010bc215d77615412
Wildan <wildanhuang@gmail.com>
2024-08-05 11:17:05 +0700

    add ssl to all env

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Stt
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << "sttbaptisbandung.ac.id"
+    config.force_ssl = true
   end
 end
