0add028c66245c4db2e8a6d5a6a78f3fc7c45b5a
Wildan <wildanhuang@gmail.com>
2024-08-07 15:32:17 +0700

    host for developing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   
   config.action_controller.raise_on_missing_callback_actions = true
 
-  # config.hosts << "local-ubuntu:3000"
+  config.hosts << "local-ubuntu:3000"
   config.hosts << "localhost:3000"
   config.hosts << "localhost"
   config.hosts << "sttbaptisbandung.ac.id"
